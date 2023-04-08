10c5eeaafea757aee99161ff4953e095a38f047c
aldairandre <aldairandre99@gmail.com>
2023-04-08 13:00:35 +0100

    ✨ Reading input user

--- bin/learning_dart.dart
+++ bin/learning_dart.dart
@@ -1,4 +1,6 @@
 import 'package:learning_dart/src/1-varibles/index.dart' show varibles;
+import 'package:learning_dart/src/4-user-input/index.dart' show inputs;
 void main(List<String> arguments) {
-  varibles();
+  //varibles();
+  inputs();
 }
---
+++ lib/src/4-user-input/index.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+inputs() {
+  print("Enter your Name: ");
+  String? name = stdin.readLineSync();
+
+  print("What is your Age : ");
+  int age = int.parse(stdin.readLineSync()!); 
+
+  print("What is your height: ");
+  double height = double.parse(stdin.readLineSync()!); 
+
+
+  print("Name $name");
+  print("Age $age years old");
+  print("Height $height");
+}
